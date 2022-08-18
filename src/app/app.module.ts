61deb0d0ab3c897844b0e90d6635cd1051ad8ab9
Taras <tarasparakhin@gmail.com>
2022-08-18 16:09:24 +0300

    Figma layout "Torty Tut" for Hebron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AdvantageComponent } from './components/advantage/advantage.component';
+import { EventsComponent } from './components/events/events.component';
+import { ConfectionersComponent } from './components/confectioners/confectioners.component';
+import { OrderComponent } from './components/order/order.component';
+import { ManufactoryComponent } from './components/manufactory/manufactory.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AdvantageComponent,
+    EventsComponent,
+    ConfectionersComponent,
+    OrderComponent,
+    ManufactoryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
