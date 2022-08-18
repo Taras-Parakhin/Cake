3cfdb7aa14484bb3feb0cb7b1212e5032906bb83
Taras <tarasparakhin@gmail.com>
2022-08-18 16:12:15 +0300

    Figma layout "Torty Tut" for Hebron

---
+++ src/app/components/confectioners/confectioners.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confectioners',
+  templateUrl: './confectioners.component.html',
+  styleUrls: ['./confectioners.component.scss']
+})
+export class ConfectionersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
